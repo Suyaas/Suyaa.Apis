34a2f615fdf6ef5f27447b610ed3b25ed229262f
inmount <inmount@live.cn>
2023-01-16 00:27:35 +0800

    新增：添加基础和运行模块

---
+++ Suyaa.Apis.Window/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Suyaa.Apis.Window
+{
+    internal class Startup : Suyaa.Microservice.Startup
+    {
+        /// <summary>
+        /// 启动器
+        /// </summary>
+        /// <param name="configuration">配置</param>
+        public Startup(IConfiguration configuration) : base(configuration) { }
+
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+            //this.Import<Test.ModuleStartup>();
+        }
+
+        protected override void OnConfigureServices(IServiceCollection services)
+        {
+            base.OnConfigureServices(services);
+        }
+    }
+}
