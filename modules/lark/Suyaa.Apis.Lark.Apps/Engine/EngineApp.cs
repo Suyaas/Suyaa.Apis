18c540dd2286e7c19b9eb954360b48934b461ed3
inmount <inmount@live.cn>
2023-03-27 01:16:02 +0800

    新增：【Lark】 增加引擎处理应用

---
+++ modules/lark/Suyaa.Apis.Lark.Apps/Engine/EngineApp.cs
@@ -0,0 +1,18 @@
+﻿using Suyaa.Microservice.Dependency;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suyaa.Apis.Lark.Apps.Engine
+{
+    /// <summary>
+    /// 脚本引擎
+    /// </summary>
+    [App("Lark/Editor")]
+    public class EngineApp
+    {
+        
+    }
+}
