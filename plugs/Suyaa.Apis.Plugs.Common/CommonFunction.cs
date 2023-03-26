b58bafbf68a7bd3168c9689c083654e8bfdc1e21
inmount <inmount@live.cn>
2023-03-26 18:50:22 +0800

    新增：【Plugs】 添加通用插件

---
+++ plugs/Suyaa.Apis.Plugs.Common/CommonFunction.cs
@@ -0,0 +1,20 @@
+﻿using Egg.Lark;
+
+namespace Suyaa.Apis.Plugs.Common
+{
+    /// <summary>
+    /// 公共函数集合
+    /// </summary>
+    public class CommonFunction: ScriptFunctionRegistrBase
+    {
+        /// <summary>
+        /// 插件测试
+        /// </summary>
+        /// <returns></returns>
+        [Func]
+        public string PlugTest()
+        {
+            return "plug test ok";
+        }
+    }
+}
