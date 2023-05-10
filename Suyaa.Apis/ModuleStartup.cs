0cb5aef18d7e0a3691820103887716c975dd6e1b
Lin Zheng <inmount@live.cn>
2023-05-10 20:21:50 +0800

    修改：变更配置

--- Suyaa.Apis/ModuleStartup.cs
+++ Suyaa.Apis/ModuleStartup.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Suyaa.Apis.Dependency;
 
 namespace Suyaa.Apis
 {
@@ -19,6 +20,10 @@ namespace Suyaa.Apis
             // 注册Api管理器
             var apiManager = new ApiManager();
             services.AddSingleton<IApiManager>(apiManager);
+
+            // 注册数据库连接
+            var connections = new Connections();
+            services.AddSingleton<IConnections>(connections);
         }
     }
 }
